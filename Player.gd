e51853ad3106a12c9e6f13407953faf3c9df19f0
D.KOFY <whitebarb.dz@gmail.com>
2021-11-27 21:10:55 +0100

    camera + sprite + anim

---
+++ Player.gd
@@ -0,0 +1,42 @@
+extends KinematicBody2D
+
+const UP = Vector2(0,-1)
+const GRAVITY = 20
+const MAXFALLSPEED = 200
+const MAXSPEED = 80
+
+
+var motion = Vector2()
+var facing_right = true
+
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	motion.y += GRAVITY
+	
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
+		
+	if facing_right == true:
+		$Pngegg.scale.x = 1
+	else:
+		$Pngegg.scale.x = -1
+	if Input.is_action_pressed("right"):
+		motion.x = MAXSPEED
+		facing_right =true
+		$AnimationPlayer.play("go left")
+	if Input.is_action_pressed("left"):
+		motion.x = -MAXSPEED
+	else:
+		motion.x =0
+	
+	motion = move_and_slide(motion,UP)
